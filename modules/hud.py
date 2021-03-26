d24df83514890de461613a547555ebccd5dcad8d
Zavioer <piotrekq321@gmail.com>
2021-03-26 11:20:30 +0100

    Update time save and load
    Additionally add font paths

--- modules/hud.py
+++ modules/hud.py
@@ -16,6 +16,7 @@ class HUD(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = WIDTH - HUD_WIDTH
         self.padding = 30
+        self.font = pygame.font.Font(GOMARINCE, 30)
 
     def displayLvl(self, lvlNumber):
         """
@@ -25,9 +26,13 @@ class HUD(pygame.sprite.Sprite):
             Full level name in convention number.txt.
         :type lvlNumber: str, required
         """
-        cleanName = lvlNumber[:lvlNumber.find('.')]
-        font = pygame.font.Font('src/fonts/gomarice_no_continue.ttf', 32)
-        text = font.render(f'Level: {cleanName}', True, WHITE)
+
+        if not isinstance(lvlNumber, int):
+            cleanName = lvlNumber[:lvlNumber.find('.')]
+        else:
+            cleanName = str(lvlNumber)
+
+        text = self.font.render(f'Level: {cleanName}', True, WHITE)
         levelPosition = text.get_rect(y=self.padding, centerx=self.image.get_width() / 2)
 
         self.image.blit(text, levelPosition)
@@ -40,8 +45,8 @@ class HUD(pygame.sprite.Sprite):
             Nick name to display on the HUD.
         :type passedName: str, required
         """
-        font = pygame.font.Font('src/fonts/gomarice_no_continue.ttf', 30)
-        text = font.render(f'{passedName.title()}', True, RED)
+        font = pygame.font.Font(GOMARINCE, 30)
+        text = self.font.render(f'{passedName.title()}', True, RED)
 
         playerNamePosition = text.get_rect(y=self.padding * 2, centerx=self.image.get_width() / 2)
 
@@ -67,34 +72,35 @@ class HUD(pygame.sprite.Sprite):
             Amount of points to display on the HUD.
         :type passedPoints: int, required
         """
-        font = pygame.font.Font('src/fonts/gomarice_no_continue.ttf', 30)
-        text = font.render(f'Score: {passedPoints}', True, WHITE)
+        font = pygame.font.Font(GOMARINCE, 30)
+        text = self.font.render(f'Score: {passedPoints}', True, WHITE)
         pointsPosition = text.get_rect(y=self.padding * 4, centerx=self.image.get_width() / 2)
         self.image.blit(text, pointsPosition)
 
 
 class Timer(pygame.sprite.Sprite):
-    def __init__(self, start, fontName):
+    def __init__(self, start, previousTime=0):
         """
         Class which returns the time (ticks) since the game start.
 
         :param start:
             Start amount of ticks.
         :type start: int, required
-        :param fontName:
-            Name of the font.
-        :type fontName: pygame.Font, required
+        :param previousTime:
+            Variable for adding time if current game is loaded from save.
+        :type previousTime: int, required
         """
         pygame.sprite.Sprite.__init__(self)
         self.start = start
         self.passedTime = 0
         self.passedTimeStr = ''
-        self.fontName = pygame.font.Font('src/fonts/gomarice_no_continue.ttf', 32)
+        self.fontName = pygame.font.Font(GOMARINCE, 32)
         self.x = 0
         self.y = 0
         self.buffer = 0
         self.startPause = 0
         self.endTime = 0
+        self.previousTime = previousTime
 
     def update(self, ticks):
         """
@@ -104,7 +110,10 @@ class Timer(pygame.sprite.Sprite):
             Current value of ticks.
         :type ticks: int, required
         """
-        self.passedTime = round((ticks - self.start) / 1000)
+        if self.previousTime > 0:
+            self.passedTime = round(((ticks - self.start) + self.previousTime) / 1000)
+        else:
+            self.passedTime = round((ticks - self.start) / 1000)
 
         if self.buffer > 0:
             self.passedTime -= self.buffer
@@ -152,7 +161,7 @@ class Timer(pygame.sprite.Sprite):
         :type ticks: int, required
         """
         self.startPause = ticks
-        self.endTime = self.passedTime * 1000
+        self.endTime = (ticks - self.start) + self.previousTime
 
     def resume(self, ticks):
         """
--- modules/logic.py
+++ modules/logic.py
@@ -31,8 +31,6 @@ def startTheGame(screen, lvlName, game, points, flag):
         Flag that allows to discriminate between module I, II and III.
     :type flag: str, required
     """
-    myFont = pygame.font.SysFont('Montserrat', 30)
-
     # Initial sprites groups and map floor
     walls = pygame.sprite.Group()
     boxes = pygame.sprite.Group()
@@ -103,9 +101,10 @@ def startTheGame(screen, lvlName, game, points, flag):
     destinationsAmount = len(destinations.sprites())
 
     if flag == RESTORE:
-        gamerTimer = Timer(game.restoreDetails['endTime'], myFont)
+        gamerTimer = Timer(pygame.time.get_ticks(), game.restoreDetails['endTime'])
     else:
-        gamerTimer = Timer(pygame.time.get_ticks(), myFont)
+        gamerTimer = Timer(pygame.time.get_ticks())
+
     hud = HUD(gamerTimer)
 
     allSprites = pygame.sprite.Group(floors, walls, destinations, boxes, storekeeper)
@@ -124,6 +123,8 @@ def startTheGame(screen, lvlName, game, points, flag):
                 game.currentPlayerState['height'] = mapHeight
                 game.currentPlayerState['sprites'] = allSprites
                 game.currentPlayerState['time'] = hud.timer.endTime
+
+                print(f'END TIME {hud.timer.endTime}')
 
                 if flag == RESTORE:
                     game.currentPlayerState['flag'] = game.restoreDetails['flag']
@@ -179,18 +180,19 @@ def startTheGame(screen, lvlName, game, points, flag):
         if len(placedBoxes) == destinationsAmount:
             game.display.fill(BLACK)
             game.drawText('You passed the level!', 65, midWidth, midHeight,
-                               WHITE, game.fontName)
+                          WHITE, game.fontName)
             game.window.blit(game.display, (0, 0))
             pygame.display.update()
             game.resetKeys()
             time.sleep(0.6)
 
-            if flag == MODULE_I or flag == MODULE_III:
+            if flag == MODULE_I or flag == MODULE_III or \
+                    (flag == RESTORE and game.restoreDetails['flag'] == MODULE_III):
                 game.logicState = False
                 game.currentMenu = game.mainMenu
                 game.currentMenu.displayMenu()
 
-            if flag == MODULE_II:
+            if flag == MODULE_II or (flag == RESTORE and game.restoreDetails['flag'] == MODULE_II):
                 game.logicState = False
                 game.gameLevel += 1
 
@@ -218,7 +220,7 @@ def startTheGame(screen, lvlName, game, points, flag):
         else:
             hud.displayLvl(lvlName)
 
-        if flag == MODULE_II:
+        if flag == MODULE_II or flag == RESTORE:
             hud.displayPoints(points)
 
         hud.displayPlayerName(game.playerName)
@@ -229,7 +231,7 @@ def startTheGame(screen, lvlName, game, points, flag):
         pygame.display.flip()
 
 
-def saveBoard(width, height, sprites, endTime, playerName, lvlName, gamePoints, flag):
+def saveGame(width, height, sprites, endTime, playerName, lvlName, gamePoints, flag):
     """
     Function for saving current playing lvl and additional information in to
     shelve file.
